32bac64561639278fb91ce068a5a1b13a052627b
Andrew Kos <akos123@gmail.com>
2016-09-28 23:31:42 -0500

    cache for api"

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -1,14 +1,21 @@
 !(function (App) {
 
+  var cardCache;
+
   App.Api = {
     getSets: function (endpoint) {
       return new Promise(function (resolve, reject) {
+        if (cardCache) {
+          resolve(cardCache);
+        }
+
         var request = new XMLHttpRequest();
 
         request.addEventListener('readystatechange', function () {
           if (this.readyState === XMLHttpRequest.DONE) {
             if (this.status === 200) {
-              resolve(JSON.parse(this.response));
+              cardCache = JSON.parse(this.response);
+              resolve(cardCache);
             } else {
               reject();
             }

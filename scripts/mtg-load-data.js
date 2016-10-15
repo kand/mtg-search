d64ef9ccdf82b58589b806cc8b174108ec0ada35
Andrew Kos <akos123@gmail.com>
2016-10-15 13:10:59 -0500

    load cards

--- scripts/mtg-load-data.js
+++ scripts/mtg-load-data.js
@@ -3,12 +3,27 @@ let setData = require('mtgjson/data/AllSets.json');
 let gun = require('gun')();
 
 let sets = gun.get('sets');
+let cards = gun.get('cards');
 
+// loop through sets once to initialize
 Object.keys(setData).forEach(function (setKey) {
-  sets.path(setKey).put({ name: setData[setKey].name });
+  let set = setData[setKey];
+
+  sets.path(setKey).put({ name: set.name });
 });
 
-sets.map(function (v, k) {
-  console.log(v.name, k)
+// loop through cards in each set so they can ref their printings
+Object.keys(setData).forEach(function (setKey) {
+  let set = setData[setKey];
+  let setInDb = sets.get(setKey);
+
+  set.cards.forEach(function (card) {
+    let cardInDb = cards.path(setKey + '-' + card.number).put({
+      name: card.name
+    });
+
+    setInDb.path('cards.' + card.number).put(cardInDb);
+    cardInDb.path('printings.' + setKey).put(sets.path(setKey));
+  }); 
 });
 

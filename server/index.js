e29a8a54e43cdf067a4cdcbbd2943a65223b4a72
Andrew Kos <akos123@gmail.com>
2016-10-19 23:20:07 -0500

    some filtering

--- server/index.js
+++ server/index.js
@@ -6,6 +6,30 @@ let app = express();
 
 app.get('/sets/:setAbbrv/', (req, res) => {
   let data = JSON.parse(fs.readFileSync(`./server/data/${req.params.setAbbrv}.json`, 'utf8'));
+  let queryKeys = Object.keys(req.query);
+
+  if (queryKeys.length > 0) {
+
+    data.cards = data.cards.filter((card) => {
+      return queryKeys.reduce((include, paramName) => {
+
+        let cardValue = card[paramName];
+        let queryValue = req.query[paramName];
+        let propType = typeof card[paramName];
+
+        if (propType === 'string') {
+          return include && cardValue.indexOf(queryValue) > -1;
+        } else if (propType === 'number') {
+          return include && cardValue === paramName;
+        }
+
+        // TODO : include other types
+
+        return false;
+
+      }, true);
+    });
+  }
 
   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');

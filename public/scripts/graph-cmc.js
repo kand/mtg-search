f014095bf2239aff1fb923455b02d89bb00736f1
Andrew Kos <akos123@gmail.com>
2016-11-05 02:03:19 -0500

    vertical chart

--- public/scripts/graph-cmc.js
+++ public/scripts/graph-cmc.js
@@ -14,8 +14,8 @@
       return document.createElement('div');
     }
 
-    var width = 420;
-    var barHeight = 20;
+    var height = 300;
+    var width = 300; 
 
     var data = cards
         .map(function (card) {
@@ -26,31 +26,34 @@
           return costs;
         }, []);
 
-    var x = d3.scaleLinear()
+    var y = d3.scaleLinear()
         .domain([0, d3.max(data)])
-        .range([0, width]);
+        .range([height, 0]);
+
+    var barWidth = width / data.length;
 
     var svg = d3.select(container).append('svg')
-        .attr('width', width)
-        .attr('height', barHeight * data.length);
+        .attr('height', height)
+        .attr('width', width);
 
     var bar = svg.selectAll('g')
           .data(data)
         .enter().append('g')
-          .attr('transform', function (d, i) { return 'translate(0,' + i * barHeight + ')'; });
+          .attr('transform', function (d, i) { return 'translate(' + i * barWidth + ', 0)'; });
 
     bar.append('rect')
         .style('fill', 'steelblue')
-        .attr('width', function (d) { return x(d) || 0; })
-        .attr('height', barHeight - 1);
+        .attr('y', function (d) { return y(d) || 0; })
+        .attr('height', function (d) { return height - y(d) || 0; })
+        .attr('width', barWidth - 1);
 
     bar.append('text')
-        .attr('x', function (d) { return (x(d) || 3) - 3; })
-        .attr('y', barHeight / 2)
-        .attr('dy', '0.35em')
+        .attr('x', barWidth / 2)
+        .attr('y', function (d) { return height - 10; })
+        .attr('dx', '-0.5em')
+        .attr('dy', '0.75em')
         .style('fill', 'white')
         .style('font', '10px sans-serif')
-        .style('text-anchor', 'end')
         .text(function (d) { return d || 0; });
 
     return svg.node();
